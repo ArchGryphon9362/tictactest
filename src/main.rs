63b1eea86e2981cfd9c68e90aba84cd8f9269fb3
Lex Nastin <lexnastin@gmail.com>
2022-08-16 01:38:16 +0100

    refactor: replace graphics library
    fermium -> macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use macroquad::prelude as mq;
 mod graphics;
-use fermium::video::SDL_WINDOWPOS_CENTERED;
 
-fn main() {
-    let window = graphics::Window::new(String::from("TicTacTest"), 800, 600, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
-    println!("Hello, world!");
+fn init() {
+    
+}
+
+fn frame(frame_count: &mut i32) {
+    mq::clear_background(mq::BLACK);
+    mq::draw_text(&format!("Hey, {}", frame_count)[..], 30f32, 30f32, 48f32, mq::WHITE);
+    *frame_count += 1;
+}
+
+#[macroquad::main("TicTacTest")]
+async fn main() {
+    let mut frame_count = 0;
+    graphics::event_loop(init, frame, &mut frame_count).await
 }
